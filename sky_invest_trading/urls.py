b935429df7777bec8acd813471c06507ab80b757
devvourer <beksultan.toktobekov@mail.ru>
2022-06-04 15:51:01 +0600

    fix

--- sky_invest_trading/urls.py
+++ sky_invest_trading/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path('__debug__', include(debug_toolbar.urls))
 ]
 
-# if settings.DEBUG:
-#     if settings.STATIC_ROOT:
-#         urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#     if settings.MEDIA_ROOT:
-#         urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    if settings.STATIC_ROOT:
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    if settings.MEDIA_ROOT:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
